ea04c147b6323d0a4189e28db34d1d153bec375b
Aléxia <alexia.dorneles@thoughtworks.com>
2021-03-24 10:46:40 -0300

    feat: use base rxjs

--- src/components/screens/Search.tsx
+++ src/components/screens/Search.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
-import { SearchBox } from '../shared'
+import { BehaviorSubject, interval } from 'rxjs'
+import { debounce } from 'rxjs/operators'
+import { BookModel } from '../../@types'
+import { Book, SearchBox } from '../shared'
 
 export function Search(): JSX.Element {
+	const textUpdaterObserver = new BehaviorSubject('')
+
+	textUpdaterObserver.pipe(debounce(() => interval(500))).subscribe(text => console.log(text))
+
 	return (
 		<div className="Search">
-			<SearchBox />
+			<SearchBox textObserver={textUpdaterObserver} />
+			<Book book={{} as BookModel} />
 		</div>
 	)
 }
--- src/components/shared/SearchBox/SearchBox.tsx
+++ src/components/shared/SearchBox/SearchBox.tsx
@@ -1,11 +1,23 @@
-import React, { useState } from 'react'
+import React, { ChangeEvent, useState } from 'react'
+import { Observer } from 'rxjs'
 
-export function SearchBox(): JSX.Element {
+export interface SearchBoxPropTypes {
+	textObserver: Observer<string>
+}
+
+export function SearchBox({ textObserver }: SearchBoxPropTypes): JSX.Element {
 	const [text, setText] = useState('')
+
+	const onChangeText = (e: ChangeEvent<HTMLInputElement>) => {
+		const value = e.target.value
+		setText(value)
+		textObserver.next(value)
+	}
+
 	return (
 		<div>
 			<label htmlFor="">Procure o livro:</label>
-			<input type="text" value={text} onChange={e => setText(e.target.value)} />
+			<input type="text" value={text} onChange={onChangeText} />
 		</div>
 	)
 }
