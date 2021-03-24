58555f70aba118734c2ce50ad7cb364d348e2c13
Aléxia <alexia.dorneles@thoughtworks.com>
2021-03-24 10:31:50 -0300

    feat: show Search component

---
+++ src/components/screens/Search.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { SearchBox } from '../shared'
+
+export function Search(): JSX.Element {
+	return (
+		<div className="Search">
+			<SearchBox />
+		</div>
+	)
+}
