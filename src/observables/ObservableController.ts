82d1b6d1ad4dc7c78398f09c844ab78df1aa5a14
Aléxia <alexia.dorneles@thoughtworks.com>
2021-03-26 12:01:05 -0300

    feat: create new empty subjects

--- src/observables/ObservableController.ts
+++ src/observables/ObservableController.ts
@@ -1,8 +1,11 @@
-import { of, Subject } from 'rxjs'
+import { Subject } from 'rxjs'
+import { BookModel } from '../@types'
 import { ObservableStore } from './ObservableStore'
 
 export class ObservableController {
 	public static setup(): void {
 		ObservableStore.save('CurrentReading', new Subject<string>())
+		ObservableStore.save('BookTextChange', new Subject<string>())
+		ObservableStore.save('BookSearchResponse', new Subject<BookModel[]>())
 	}
 }
