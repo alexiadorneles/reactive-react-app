bee31199b068e744ed6ab92bd4e1b082e7d6b089
Aléxia <alexia.dorneles@thoughtworks.com>
2021-03-26 12:02:09 -0300

    feat: mount mediators singletons

---
+++ src/mediators/MediatorSingletonCreator.ts
@@ -0,0 +1,9 @@
+import { HttpService } from '../services/HttpService'
+import { BookSearchMediator } from './Book/BookSearchMediator'
+
+export abstract class MediatorSingletonCreator {
+	public static mountInstances(): void {
+		const googleBooksService = new HttpService('https://www.googleapis.com/books/v1/volumes?q=')
+		new BookSearchMediator(googleBooksService).mediate()
+	}
+}
