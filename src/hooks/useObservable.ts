23e6f603664415c3045a9121147739a0c1c1b753
Aléxia <alexia.dorneles@thoughtworks.com>
2021-03-26 10:34:16 -0300

    refactor: improve typing on useObservable

--- src/hooks/useObservable.ts
+++ src/hooks/useObservable.ts
@@ -1,11 +1,32 @@
 import { Dispatch, SetStateAction } from 'react'
 import { Subscription } from 'rxjs'
-import { ReactiveData, ReactiveKeys } from '../@types/Observables'
+import { ReactiveData, ReactiveKeys, ReactiveKeyToData } from '../@types/Observables'
 import { ObservableStore } from '../observables/ObservableStore'
+
+type SubscribeFunction<T> = (
+	next?: ((value: T) => void) | undefined,
+	error?: ((error: unknown) => void) | undefined,
+	complete?: (() => void) | undefined,
+) => Subscription
+
+export function useObservable<T extends keyof ReactiveKeyToData, R extends ReactiveKeyToData[T]>(
+	observableName: T,
+	dispatcher: Dispatch<SetStateAction<R>>,
+): Subscription
+
+export function useObservable<T extends keyof ReactiveKeyToData, R extends ReactiveKeyToData[T]>(
+	observableName: T,
+): SubscribeFunction<R>
 
 export function useObservable<T extends ReactiveData>(
 	observableName: ReactiveKeys,
-	dispatcher: Dispatch<SetStateAction<T>>,
-): Subscription {
-	return ObservableStore.getObservable(observableName).subscribe(value => dispatcher(value as T))
+	dispatcher?: Dispatch<SetStateAction<T>>,
+): Subscription | SubscribeFunction<T> {
+	const observable = ObservableStore.getObservable(observableName)
+
+	if (dispatcher) {
+		return observable.subscribe(value => dispatcher(value as T))
+	}
+
+	return observable.subscribe as SubscribeFunction<T>
 }
